6dd488683b6e37df4830c0d6c9e8e0f1b5c28bc6
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 21:41:02 +0530

    Add files via upload

---
+++ mprogram3.java
@@ -0,0 +1,35 @@
+//Swapping of numbers by 3 ways 
+import java.util.*;
+class mprogram3
+{
+    public void main()
+    {
+        
+        // FIRST LOGIC TO SWAP THE NUMBER USING THE THIRD VARIABLE
+        /*int a = 10,b= 20,c=0;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);*/
+        
+        // second logic to swap 2 number using the + and - operator 
+        /*int a = 10,b=20;
+        a = a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);*/
+        
+        //third logic is to swap the 2 numbers using multiply and divide (but the number should not be = o 
+        int a = 10,b=20;
+         a= a*b;
+         b=a/b;
+         a=a/b;
+         System.out.println(a);
+        System.out.println(b);
+    
+    }
+    
+}
+        
